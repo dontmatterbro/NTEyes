eb9dc577b788f66876b3d113245a53e00e02ae9e
dontmatterbro <yh846175@gmail.com>
2024-07-17 15:44:00 +0300

    general fixes

--- Lua/Autorun/init.lua
+++ Lua/Autorun/init.lua
@@ -1,19 +1,11 @@
----@diagnostic disable: lowercase-global, undefined-global <----- idk what this is
-
---Adds NT Eyes as a Neurotrauma expansion.
-NTEYE = {} -- NT Eyes
---NTEYE = {} -- this existing should make surgical skill gain work? <--- check this out for NT Surgery comp
-NTEYE.Name="NT Eyes"
-NTEYE.Version = "A1.0.0"
---NTEYE.VersionNum = 01020401  <------ idk what this is we need to ask guns
+NTEYE = {} -- Neurotrauma Eyes
+NTEYE.Name="Eyes"
+NTEYE.Version = "A1.0"
+NTEYE.VersionNum = 01000000
 NTEYE.MinNTVersion = "A1.9.4h1"
 NTEYE.MinNTVersionNum = 01090401
 NTEYE.Path = table.pack(...)[1]
-Timer.Wait(function() if NTC ~= nil and NTC.RegisterExpansion ~= nil then NTC.RegisterExpansion(NTEYE) end end,1)
-
-
---if Game.IsMultiplayer and CLIENT then return end <------ causes issues better to assign it to scripts individualy 
-
+Timer.Wait(function() if NTC ~= nil then NTC.RegisterExpansion(NTEYE) end end,1)
 
 -- Checks if NT Eyes is enabled
 local enabled = Game.GetEnabledContentPackages()
@@ -29,6 +21,6 @@ if isEnabled then
     dofile(myModPath .. "/Lua/Scripts/humanstuff.lua")
 --    dofile(myModPath .. "/Lua/Scripts/eyesurgery.lua")
 --   dofile(myModPath .. "/Lua/Scripts/eyeupdate.lua")
---    dofile(myModPath .. "/Lua/Scripts/helperfunctions.lua") I don't we need this, lua probably can use the NT code
+--    dofile(myModPath .. "/Lua/Scripts/helperfunctions.lua") I don't think we need this, lua probably can use the NT code
 --	dofile(myModPath .. "/Lua/Scripts/newhumanstuff.lua") <----- we will switch to this once the code is optimized
 end
