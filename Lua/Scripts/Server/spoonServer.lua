a97571eaf6634a7c06586f5343c2754d198d6d8f
dontmatterbro <yh846175@gmail.com>
2024-08-16 15:35:34 +0300

    spoon1

--- Lua/Scripts/Server/spoonServer.lua
+++ Lua/Scripts/Server/spoonServer.lua
@@ -1,69 +1,90 @@
-
+--Extraction from dead monsters
 Hook.Add('spoonUsed', 'eyestealing', function(effect, dt, item, targets, targetCharacter, worldpos)
 
+	for k, spoontarget in pairs(targets) do
+		
+		if	--human eyes
+			spoontarget.IsHuman
+		then 
+			if 
+					not HF.HasAffliction(spoontarget, "noeye") 
+				and not HF.HasAffliction(spoontarget, "th_amputation") 
+				and	not NTEYE.IsInDivingGear(spoontarget) 
+				and not HF.HasAffliction(spoontarget,"stasis",0.1) 
+			then
+				HF.AddAfflictionLimb(spoontarget, "noeye", 11, 2)
+				HF.AddAfflictionLimb(spoontarget, "traumaticshock", 11, 50)
+				HF.AddAfflictionLimb(spoontarget, "bleeding", 11, math.random(1,50))
+				
+				--item.Condition = 10
+			end
 
-	for k, v in pairs(targets) do
-  
-		if --monster eyes
-			v.SpeciesName == "Mudraptor" 
-			or v.SpeciesName == "Crawler" 
-			or v.SpeciesName == "Hammerhead" 
-		then
-		
-			if 
-				not HF.HasAffliction(v, "noeye") 
-			then
+
+		else
+
 			
-				HF.AddAfflictionLimb(v, "noeye", 11, 2)
-				
-				Timer.Wait(function()
-					local prefab = ItemPrefab.GetItemPrefab("transplant_eyes_monster")
-					Entity.Spawner.AddItemToSpawnQueue(prefab, item.WorldPosition, nil, nil, function(item) end)
-					Entity.Spawner.AddItemToRemoveQueue(item)
-				end, 1)
-				
-			end
 		  
-		  
-		elseif --husk eyes
-			v.SpeciesName == "Husk" 
-			or v.SpeciesName == "Crawlerhusk" 
-		then
-		
-			if 
-				not HF.HasAffliction(v, "noeye") 
+			if --monster eyes
+				spoontarget.SpeciesName == "Mudraptor" 
+				or spoontarget.SpeciesName == "Crawler" 
+				or spoontarget.SpeciesName == "Hammerhead" 
 			then
-			
-				HF.AddAfflictionLimb(v, "noeye", 11, 2)
 				
-				Timer.Wait(function()
-					local prefab = ItemPrefab.GetItemPrefab("transplant_eyes_husk")
-					Entity.Spawner.AddItemToSpawnQueue(prefab, item.WorldPosition, nil, nil, function(item) end)
-					Entity.Spawner.AddItemToRemoveQueue(item)
-				end, 1)
+				if 
+					not HF.HasAffliction(spoontarget, "noeye") 
+				then
+					
+					HF.AddAfflictionLimb(spoontarget, "noeye", 11, 2)
+						
+					Timer.Wait(function()
+						local prefab = ItemPrefab.GetItemPrefab("transplant_eyes_monster")
+						Entity.Spawner.AddItemToSpawnQueue(prefab, item.WorldPosition, nil, nil, function(item) end)
+						Entity.Spawner.AddItemToRemoveQueue(item)
+					end, 1)
+						
+				end
+				  
+				  
+			elseif --husk eyes
+				spoontarget.SpeciesName == "Husk" 
+				or spoontarget.SpeciesName == "Crawlerhusk" 
+			then
+				
+				if 
+					not HF.HasAffliction(spoontarget, "noeye") 
+				then
+					
+					HF.AddAfflictionLimb(spoontarget, "noeye", 11, 2)
+						
+					Timer.Wait(function()
+						local prefab = ItemPrefab.GetItemPrefab("transplant_eyes_husk")
+						Entity.Spawner.AddItemToSpawnQueue(prefab, item.WorldPosition, nil, nil, function(item) end)
+						Entity.Spawner.AddItemToRemoveQueue(item)
+					end, 1)
+						
+				end
+				
+				
+			elseif --terror eyes
+				spoontarget.SpeciesName == "Charybdis" 
+				or spoontarget.SpeciesName == "Latcher" 
+			then
+				
+				if 
+					not HF.HasAffliction(spoontarget, "noeye") 
+				then
+					
+					HF.AddAfflictionLimb(spoontarget, "noeye", 11, 2)
+						
+					Timer.Wait(function()
+						local prefab = ItemPrefab.GetItemPrefab("transplant_eyes_terror")
+						Entity.Spawner.AddItemToSpawnQueue(prefab, item.WorldPosition, nil, nil, function(item) end)
+						Entity.Spawner.AddItemToRemoveQueue(item)
+					end, 1)
+				end
 				
 			end
-		
-		
-		elseif --terror eyes
-			v.SpeciesName == "Charybdis" 
-			or v.SpeciesName == "Latcher" 
-		then
-		
-			if 
-				not HF.HasAffliction(v, "noeye") 
-			then
-			
-				HF.AddAfflictionLimb(v, "noeye", 11, 2)
-				
-				Timer.Wait(function()
-					local prefab = ItemPrefab.GetItemPrefab("transplant_eyes_terror")
-					Entity.Spawner.AddItemToSpawnQueue(prefab, item.WorldPosition, nil, nil, function(item) end)
-					Entity.Spawner.AddItemToRemoveQueue(item)
-				end, 1)
-			end
-		
-		end
+		end 
 
 --[[
 	if v.SpeciesName == "Human" then
@@ -76,15 +97,19 @@ Hook.Add('spoonUsed', 'eyestealing', function(effect, dt, item, targets, targetC
 				--HF.CanPerformSurgeryOn(targetCharacter) 
 				not NTEYE.IsInDivingGear(v) 
 				and not HF.HasAffliction(v,"stasis",0.1) 
-				and not HF.HasAffliction(v, "robotspawned") 
+
 				
 			then print("2")
 				Entity.Spawner.AddItemToSpawnQueue(NTEYE.GiveItemBasedOnEye(v), item.WorldPosition, nil, nil, function(item) end) 
 				Entity.Spawner.AddItemToRemoveQueue(item)
 			print("3")
 			end
-		end
-	end --]]
+		end--]]
+	
 
 	end 
+	
 end) 
+
+
+
