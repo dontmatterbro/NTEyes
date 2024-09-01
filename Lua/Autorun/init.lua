7e7052a6cad05a215002313ec2139b1be925721a
dontmatterbro <yh846175@gmail.com>
2024-09-01 21:31:20 +0300

    v 1.1.16

--- Lua/Autorun/init.lua
+++ Lua/Autorun/init.lua
@@ -1,7 +1,7 @@
 NTEYE = {} -- Neurotrauma Eyes
 NTEYE.Name="Eyes"
-NTEYE.Version = "A1.1.15h5"
-NTEYE.VersionNum = 010101505
+NTEYE.Version = "A1.1.16"
+NTEYE.VersionNum = 010101600
 NTEYE.MinNTVersion = "A1.9.4h1"
 NTEYE.MinNTVersionNum = 01090401
 NTEYE.Path = table.pack(...)[1]
