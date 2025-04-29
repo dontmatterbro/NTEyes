a42f4d2cdc156d1f7598cb3aa3fd088ef7e239ca
dontmatterbro <yh846175@gmail.com>
2025-04-29 16:40:17 +0300

    additions
    human eye damage framework
    visual indicator frame work
    added loswarning.lua back, does not seem like awful code

--- Lua/Scripts/Server/humanupdate.lua
+++ Lua/Scripts/Server/humanupdate.lua
@@ -1,8 +1,22 @@
+--set local functions
+local function PressureDamageCalculation(c)
+	if c.IsProtectedFromPressure or c.IsImmuneToPressure then
+		return 0
+	else
+		return 1
+	end
+end
+
+local function HasEyes(c)
+	--placeholder
+end
+
 --define afflictions for humanupdate
-NTEYE.Afflictions = {
+NT.Afflictions = {
 
 	--surgery afflictions
 	sr_removedeyes = {},
+	sr_removedeye = {},
 	sr_heldlid = {},
 	sr_poppedeye = {},
 	sr_eyeconnector = {},
@@ -12,24 +26,42 @@ NTEYE.Afflictions = {
 	sr_lasersurgery = {},
 	--mechanical afflictions
 	mc_deadeyes = {},
-	mc_oneeye = {},
-	mc_eyedamage = {
-		update = function(c, i)
-			if c.stats.stasis then
-				return
-			end
-			c.afflictions[i].strength = NT.organDamageCalc(
-				c,
-				c.afflictions.mc_eyedamage.strength
-					+ NTC.GetMultiplier(c.character, "eyedamagegain") * c.stats.neworgandamage
-			)
-		end,
-	},
+	mc_deadeye = {},
+	mc_eyedamage = {},
 	mc_retinopathy = {},
 	mc_cataract = {},
 	mc_visionsickness = {},
 	mc_mismatch = {},
 	--eye type afflictions
+	et_human = {
+		max = 100,
+		update = function(c, i)
+			if c.stats.stasis then
+				return
+			end
+
+			local gain = (
+				-0.1 * c.stats.healingrate -- passive regen
+				+ c.afflictions.hypoxemia.strength / 100 -- from hypoxemia
+				+ HF.Clamp(c.afflictions.stroke.strength, 0, 20) * 0.1 -- from stroke
+				+ c.afflictions.sepsis.strength / 100 * 0.4 -- from sepsis
+				+ PressureDamageCalculation(c) -- from pressure
+			) * NT.Deltatime
+
+			if gain > 0 then
+				gain = gain
+					* NTC.GetMultiplier(c.character, "eyedamagegain") -- NTC multiplier
+					* NTConfig.Get("NT_eyedamageGain", 1) -- Config multiplier
+			end
+			c.afflictions[i].strength = c.afflictions[i].strength + gain
+
+			if (c.afflictions.mc_deadeye or c.afflictions.sr_removedeye) and c.afflictions[i].strength >= 50 then
+				c.afflictions[i].strength = 50
+			else
+				c.afflictions[i].strength = HF.Clamp(c.afflictions[i].strength, 0, 100)
+			end
+		end,
+	},
 	et_cyber = {},
 	et_enhanced = {},
 	et_plastic = {},
---
+++ Lua/Scripts/Server/loswarning.lua
@@ -0,0 +1,129 @@
+--warns the players if LoS isn't opaque
+Hook.Add("roundStart", "NTEYE.loswarning", function()
+	if CLIENT or (Game.ServerSettings == nil) or (Game.ServerSettings.LosMode == 2) then
+		return
+	end --ensure this only runs when needed
+
+	Timer.Wait(function() --do this 15~ seconds later to account for loading times
+		for key, client in pairs(Client.ClientList) do
+			if Game.ServerSettings.LosMode == 0 then
+				local chatMessage = ChatMessage.Create(
+					"NT Eyes",
+
+					"\n"
+						.. "‖color:255,100,100‖"
+						.. "Line of Sight mode is set to"
+						.. "‖color:end‖"
+						.. "‖color:255,75,75‖"
+						.. " none."
+						.. "‖color:end‖"
+						.. "\n"
+						.. "‖color:200,200,200‖"
+						.. "For intended experince set it opaque by typing"
+						.. "‖color:end‖"
+						.. "‖color:100,255,100‖"
+						.. "  !LOS "
+						.. "‖color:end‖"
+						.. "‖color:200,200,200‖"
+						.. " in the chat."
+						.. "‖color:end‖",
+					ChatMessageType.Default,
+					nil,
+					nil
+				)
+
+				Game.SendDirectChatMessage(chatMessage, client)
+			else
+				local chatMessage = ChatMessage.Create(
+					"NT Eyes",
+
+					"\n"
+						.. "‖color:255,100,100‖"
+						.. "Line of Sight mode is set to"
+						.. "‖color:end‖"
+						.. "‖color:255,75,75‖"
+						.. " translucent."
+						.. "‖color:end‖"
+						.. "\n"
+						.. "‖color:200,200,200‖"
+						.. "For intended experince set it opaque by typing"
+						.. "‖color:end‖"
+						.. "‖color:100,255,100‖"
+						.. "  !LOS "
+						.. "‖color:end‖"
+						.. "‖color:200,200,200‖"
+						.. " in the chat."
+						.. "‖color:end‖",
+					ChatMessageType.Default,
+					nil,
+					nil
+				)
+
+				Game.SendDirectChatMessage(chatMessage, client)
+			end
+		end
+	end, 15000)
+end)
+
+--sets the los to opaque if the client has perms
+Hook.Add("chatMessage", "NTEYE.setlos", function(message, client)
+	if CLIENT then
+		return
+	end --ensure this doesnt run on singleplayer
+
+	if message == "!los" or message == "!LOS" then
+		if client.HasPermission(512) then
+			if Game.ServerSettings.LosMode == 2 then
+				Timer.Wait(function()
+					local chatMessage = ChatMessage.Create(
+						"NT Eyes",
+
+						"‖color:0,255,0‖" .. "Line of Sight mode is already set to opaque." .. "‖color:end‖",
+						ChatMessageType.Default,
+						nil,
+						nil
+					)
+
+					Game.SendDirectChatMessage(chatMessage, client)
+				end, 20)
+			else
+				Game.ServerSettings.LosMode = 2
+				Game.ServerSettings.ForcePropertyUpdate()
+
+				Timer.Wait(function()
+					local chatMessage = ChatMessage.Create(
+						"NT Eyes",
+
+						"‖color:0,255,0‖"
+							.. "Line of Sight mode has been set to opaque."
+							.. "‖color:end‖"
+							.. "\n"
+							.. "‖color:0,255,0‖"
+							.. "Changes will apply at the beginning of the next round."
+							.. "‖color:end‖",
+						ChatMessageType.Default,
+						nil,
+						nil
+					)
+
+					Game.SendDirectChatMessage(chatMessage, client)
+				end, 20)
+			end
+		else
+			Timer.Wait(function()
+				local chatMessage = ChatMessage.Create(
+					"NT Eyes",
+
+					"‖color:255,0,0‖"
+						.. "You do not have permission (Manage Settings) to use this command."
+						.. "‖color:end‖",
+					ChatMessageType.Default,
+					nil,
+					nil
+				)
+
+				Game.SendDirectChatMessage(chatMessage, client)
+			end, 20)
+		end
+	end
+end)
